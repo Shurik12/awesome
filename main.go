73afc4131545d7ebf43f57faf85cdd630bd77193
user <user@WKS-187.cloudx.group>
2024-11-14 19:15:33 +0300

    Returned to http

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,11 +10,6 @@ import (
 	"github.com/Shurik12/awesomethingsshop/admin"
 )
 
-var (
-	CertFilePath = "./server.crt"
-	KeyFilePath  = "./server.key"
-)
-
 func main() {
 	// CMS server
 	port := os.Getenv("PORT")
@@ -23,26 +17,13 @@ func main() {
 		port = "9000"
 	}
 
-	// load tls certificates
-	serverTLSCert, err := tls.LoadX509KeyPair(CertFilePath, KeyFilePath)
-	if err != nil {
-		log.Fatalf("Error loading certificate and key file: %v", err)
-	}
-
-	// Configurre server to trust TLS client cert issued by your CA.
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{serverTLSCert},
-	}
-
 	cmsMux := admin.InitApp()
 	cmsServer := &http.Server{
-		Addr:      ":" + port,
-		Handler:   cmsMux,
-		TLSConfig: tlsConfig,
+		Addr:    ":" + port,
+		Handler: cmsMux,
 	}
-	// go cmsServer.ListenAndServe()
-	go cmsServer.ListenAndServeTLS(CertFilePath, KeyFilePath)
-	fmt.Println("CMS Served at https://localhost:" + port + "/admin")
+	go cmsServer.ListenAndServe()
+	fmt.Println("CMS Served at http://localhost:" + port + "/admin")
 
 	// Publish server
 	u, _ := url.Parse(os.Getenv("PUBLISH_URL"))
@@ -52,11 +33,9 @@ func main() {
 	}
 	publishMux := http.FileServer(http.Dir(admin.PublishDir))
 	publishServer := &http.Server{
-		Addr:      ":" + publishPort,
-		Handler:   publishMux,
-		TLSConfig: tlsConfig,
+		Addr:    ":" + publishPort,
+		Handler: publishMux,
 	}
-	fmt.Println("Publish Served at https://localhost:" + publishPort)
-	// log.Fatal(publishServer.ListenAndServe())
-	log.Fatal(publishServer.ListenAndServeTLS(CertFilePath, KeyFilePath))
+	fmt.Println("Publish Served at http://localhost:" + publishPort)
+	log.Fatal(publishServer.ListenAndServe())
 }
